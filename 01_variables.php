7bd6416c79d172ecb93a08297f4492f10ee8bd42
Ferran <ferran.docencia@gmail.com>
2026-05-26 18:42:19 +0200

    2026-05-26

--- 01_variables.php
+++ 01_variables.php
@@ -23,6 +23,7 @@ echo "<br>" . $saludo;
 $variable1 = 1;
 
 $variable1 = True;
+$variable1 = true;
 
 const PI1 = 3.14;
 define("PI2", 3.14);
@@ -33,5 +34,32 @@ $num1 = 1;
 $num2 = 2;
 echo "Suma de dos números: ". ($num1 + $num2);
 
+echo "<br>";
 
-?>
+
+$array_indexado = ['1', 2, True, [4, 5, 6]];
+var_dump($array_indexado);
+echo "<br>";
+print_r($array_indexado);
+echo "<br>";
+
+echo gettype($variable1);
+echo "<br>";
+echo gettype(PI1);
+echo "<br>";
+$array_asociativo = ["nombre" => "Pepe"];
+var_dump($array_asociativo);
+
+$array_asociativo['nombre'] = "Maria";
+echo "<br>";
+echo $array_asociativo['nombre'];
+$array_asociativo['edad'] = 25;
+echo "<br>";
+var_dump($array_asociativo);
+echo "<br>";
+
+
+foreach ($array_asociativo as $item) {
+    echo $item."<br>";
+}
+
