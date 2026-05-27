af012332cee5c77b2d463a695c98e2a1894ae5c4
Ferran <ferran.docencia@gmail.com>
2026-05-27 18:57:15 +0200

    2026-05-27

---
+++ 08_mini_ejercicio_02.php
@@ -0,0 +1,19 @@
+<?php
+
+// Vamos a trabajar para una frutería.
+
+$metalico = 500;
+$impuestos = 0.15; // porcentaje de la venta
+$gastos = 0.25; // porcentaje de la venta
+
+$productos = [
+    ["nombre" => "kiwi", 
+    "precioCompra" => 1.5, 
+    "precioVenta" => 2.8, 
+    "cantidadVendida" => 150, 
+    "stockActual" => 20, 
+    "unidad" => "kg" ]
+];
+
+// ¿Cuál es el beneficio actual?
+
