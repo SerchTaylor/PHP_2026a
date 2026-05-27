6a2d05813905702c64be3be770814e34ff96db1d
Ferran <ferran.docencia@gmail.com>
2026-05-27 19:22:52 +0200

    2026-05-27

--- 08_mini_ejercicio_02.php
+++ 08_mini_ejercicio_02.php
@@ -12,8 +12,30 @@ $productos = [
     "precioVenta" => 2.8, 
     "cantidadVendida" => 150, 
     "stockActual" => 20, 
-    "unidad" => "kg" ]
+    "unidad" => "kg" ],
+    ["nombre" => "manzana", 
+    "precioCompra" => 0.8, 
+    "precioVenta" => 1.7, 
+    "cantidadVendida" => 200, 
+    "stockActual" => 120, 
+    "unidad" => "kg" ],
+     ["nombre" => "fresa", 
+    "precioCompra" => 2.5, 
+    "precioVenta" => 4.9, 
+    "cantidadVendida" => 180, 
+    "stockActual" => 5, 
+    "unidad" => "kg" ],
 ];
 
-// ¿Cuál es el beneficio actual?
+// 1. ¿Cuál es el beneficio actual?
+function obtenerBeneficio() {
 
+
+}
+
+$beneficio = obtenerBeneficio();
+echo "El beneficio actual es de $beneficio €";
+
+// 2. Crea una función para vender productos.
+// Nota: el producto debe existir en el array $productos.
+// Si no es así, muestra un mensaje indicándolo.
