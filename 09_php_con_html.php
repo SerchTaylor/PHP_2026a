e908ffaded7fb514069566b62606d23a9a264821
Ferran <ferran.docencia@gmail.com>
2026-05-28 16:44:14 +0200

    2026-05-28

---
+++ 09_php_con_html.php
@@ -0,0 +1,96 @@
+<?php
+// echo "Mensaje enviado desde PHP";
+$nombre = "Michel";
+$apellido = "Parker";
+
+$hoy = new DateTime();
+$mes = $hoy->format('m');
+// echo "El mes es ".$mes;
+$fechaCompleta = $hoy->format('d-m-Y H:i:s');
+
+$arrayCoches = ['Mercedes', "SEAT", "Toyota", "Ford"];
+array_push($arrayCoches, "Audi");
+$arrayCoches[] = "BMW";
+
+// print_r($arrayCoches);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML</title>
+    <style>
+
+    </style>
+    <script>
+
+    </script>
+</head>
+
+<body>
+    <h1>Soy el fichero 09</h1>
+    <p><?php echo "Eres " . $nombre . " " . $apellido; ?></p>
+    <p><?= "Eres " . $nombre . " " . $apellido; ?></p>
+    <p style="color:darkred"><?= $fechaCompleta ?> </p>
+
+    <h2>FOREACH</h2>
+    <p>Marcas de coches</p>
+    <ul>
+        <?php foreach ($arrayCoches as $coche) { ?>
+            <li> <?= $coche; ?> </li>
+        <?php } ?>
+        <p>Otra sintaxis</p>
+        <?php foreach ($arrayCoches as $coche) : ?>
+            <li> <?= $coche; ?> </li>
+        <?php endforeach ?>
+        <p>Otra más</p>
+        <?php foreach ($arrayCoches as $coche) : ?>
+            <?= "<li>" . $coche . "</li>"; ?>
+        <?php endforeach ?>
+    </ul>
+
+    <h2>IF</h2>
+    <?php if ($nombre === "Peter") { ?>
+        <p><?= "Hola, Peter, te recuerdo que me debes 20€"; ?></p>
+    <?php } else { ?>
+        <p><?= "Hola, Pepi, gracias por el préstamo."; ?></p>
+    <?php } ?>
+    <p>otra forma de trabajar con if</p>
+    <?php if ($nombre === "Peter") : ?>
+        <p><?= "Hola, Peter, te recuerdo que me debes 20€"; ?></p>
+    <?php  else : ?>
+        <p><?= "Hola, Pepi, gracias por el préstamo."; ?></p>
+    <?php endif ?>
+
+    <h2>SWITCH</h2>
+    <?php switch($nombre) { 
+        case "Peter" :
+            echo "<p>Me debes 20€</p>";
+            break;
+        case "Pepi" :
+            echo "<p>¿Cómo estás?</p>";
+            break;
+        case "Clark":
+            echo "¿Aún no te han echado del periódico?";
+            break;
+        default:
+            echo "Hola, bienvenido/a;";
+      } ?>
+
+      <!-- 
+        Invierno : diciembre, enero, febrero
+        Primavera : marzo, abril, mayo
+        Verano : junio, julio, agosto
+        Otoño : septiembre, octubre, diciembre 
+
+        El mensaje será : Estamos en primavera
+        -->
+</body>
+
+</html>
