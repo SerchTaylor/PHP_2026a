1736ad2048306ef6787052b0c18494ee01731244
Ferran <ferran.docencia@gmail.com>
2026-06-02 20:16:39 +0200

    2026-06-02

--- registro_login/js/registro.js
+++ registro_login/js/registro.js
@@ -1,25 +1,44 @@
 let errorNombre = document.getElementById("error-nombre");
 errorNombre.classList.add("hidden");
-document.getElementById("error-email").classList.add("hidden");
-document.getElementById("error-password").classList.add("hidden");
-document.getElementById("error-edad").classList.add("hidden");
+let errorEmail = document.getElementById("error-email")
+errorEmail.classList.add("hidden");
+let errorPassword = document.getElementById("error-password")
+errorPassword.classList.add("hidden");
+let errorEdad = document.getElementById("error-edad")
+errorEdad.classList.add("hidden");
 
 // Obtener los datos del formulario
 document.getElementById("formRegistro").addEventListener("submit", (e) => {
     
     let formularioValido = true;
   
-
   let nombre = document.getElementById("nombre").value.trim();
-  alert(nombre);
-  let email = document.getElementById("email").value.trim();
+  let email = document.getElementById("email").value.trim().toLocaleLowerCase();
   let password = document.getElementById("password").value.trim();
   let edad = document.getElementById("edad").value;
   let idioma = document.getElementById("idioma").value;
 
+  
   const regexNombre = /^[a-zA-ZáéíóúàèìòùüÁÉÍÓÚÀÈÌÒÙñÑçÇ\s]{2,}$/;
   if (!regexNombre.test(nombre)) {
     errorNombre.classList.remove("hidden");
+    formularioValido = false;
+  }
+
+  const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  if (!regexEmail.test(email)){
+    errorEmail.classList.remove("hidden");
+    formularioValido = false;
+  }
+
+  const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8}$/
+  if (!regexPassword.test(password)){
+    errorPassword.classList.remove("hidden");
+    formularioValido = false;
+  }
+
+  if (parseInt(edad) < 18){
+    errorEdad.classList.remove('hidden')
     formularioValido = false;
   }
 
--- registro_login/registro.php
+++ registro_login/registro.php
@@ -5,17 +5,70 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     // Si registro.php detecta que se le ha enviado algo por POST
     // entra aquí
 
-    $nombre_raw = $_POST['nombre'];
-    $email_raw = $_POST['email'];
+    $errores = [];
+
+    // SANEADO DE LOS DATOS RECIBIDOS
+    $nombre_raw = trim($_POST['nombre']);
+    // echo $nombre_raw;
+    $nombre_saneado = htmlspecialchars(strip_tags($nombre_raw), ENT_QUOTES, "UTF-8");
+    // echo $nombre_saneado;
+
+    $email_raw = trim($_POST['email']);
+    $email_saneado = filter_var($email_raw, FILTER_SANITIZE_EMAIL);
+
     $password_raw = $_POST['password'];
+    $password_saneada = $password_raw;
+
     $edad_raw = $_POST['edad'];
+    $edad_saneada = filter_var($edad_raw, FILTER_SANITIZE_NUMBER_INT);
+
     $idioma_raw = $_POST['idioma'];
+    $idioma_saneado = htmlspecialchars(strip_tags($idioma_raw), ENT_QUOTES, "UTF-8");
 
-    print_r($_POST);
-   
+    // VALIDACIÓN 
+    $patternNombre = "/^[a-zA-ZáéíóúàèìòùüÁÉÍÓÚÀÈÌÒÙñÑçÇ\s]{2,}$/";
+    if (empty($nombre_saneado) || !preg_match($patternNombre, $nombre_saneado)) {
+        $errores[] = "El nombre no es válido. Solo se permiten letras y espacios";
+    }
+    $patternEmail = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
+    if (empty($email_saneado) || !preg_match($patternEmail, $email_saneado)) {
+        $errores[] = "El correo electrónico no tiene un formato válido";
+    }
+
+    // Comprobación de la edad
+    if (empty($edad_saneada) || intval($edad_saneada) < 18) {
+        $errores[] = "Debes ser mayor de edad";
+    }
+
+    // Comprobación de la contraseña
+    $patternPassword = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8}$/";
+    if (empty($password_saneada) || !preg_match($patternPassword, $password_saneada)) {
+        $errores[] = "La contraseña no cumple los requisitos.";
+    }
+
+    // print_r($errores);
+    if (count($errores) > 0) {
+        header("Location: registro.html");
+        exit();
+    }
+
+    echo "OK!!!!";
+
+    // Guardar los datos en un fichero externo en formato JSON
+    $nuevo_usuario = [
+        "nombre" => $nombre_saneado
+    ];
 
 
+    $archivo_json = "datos.json";
+    $usuarios_existentes = [];
 
+    if (file_exists($archivo_json)) {
+        $contenido_json = file_get_contents($archivo_json);
+        $usuarios_existentes = json_decode($contenido_json, true) ?? [];
+    }
+
+    $usuarios_existentes[] = $nuevo_usuario;
 } else {
     // Si no hay datos envíados por el formulario
     header("Location: registro.html"); // redirigimos al formulario inicial
